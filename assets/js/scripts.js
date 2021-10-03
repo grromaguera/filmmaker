c5e4aebe25ecd100eecbff01b12c1cb2e969a91c
Jim Baca <desertjim@users.noreply.github.com>
2021-10-03 16:18:00 -0400

    Setting the background color on the popup to black

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 // --------------------------------------------------------------
 	// Author       : Tanvirul Haque
 	// Template Name: Persona - Onepage CV/Resume Template
-	// Version      : 1.1	
+	// Version      : 1.1
 // --------------------------------------------------------------
 // ==============================================================
     // CSS TABLE OF CONTENTS
@@ -22,16 +22,16 @@
 
 (function ($) {
     "use strict";
-	
+
 	jQuery(document).ready(function($){
-		
+
 		// 01. START PRELOADER
 		$(window).load(function() {
 			// Animate loader off screen
 			$(".preloader").fadeOut("slow");
 		});
 		// 01. END PRELOADER
-		
+
 		// 02. START MENU BACKGROUND CHANGE JS
 		$(window).on('scroll', function () {
             if ($(window).scrollTop() > 200) {
@@ -41,7 +41,7 @@
             }
         });
 		// 02. END MENU BACKGROUND CHANGE JS
-		
+
 		// 03. START SMOTH SCROOL JS
 		$('a.smoth-scroll').on("click", function (e) {
             var anchor = $(this);
@@ -51,14 +51,14 @@
             e.preventDefault();
         });
 		// 03. END SMOTH SCROOL JS
-		
+
 		// 04. START SCROOLSPY JS
 		$('body').scrollspy({
             target: '.navbar-collapse',
             offset: 80
         });
 		// 04. END SCROOLSPY JS
-		
+
 		// 05. START TOGGLE DROPDOWN JS
 		$(document).on('click','.navbar-collapse.in',function(e) {
 		if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
@@ -66,7 +66,7 @@
 			}
 		});
 		// 05. END TOGGLE DROPDOWN JS
-		
+
 		// 06. START TYPED JS
 		$(".typed").typed({
             stringsElement: $(".typed-strings"),
@@ -74,20 +74,21 @@
             backDelay: 1500
         });
 		// 06. END TYPED JS
-		
+
 		// 07. START PORTFOLIO MIXITUP JS
 		jQuery('.grid').mixitup({
 			targetSelector: '.mix'
 		});
 		// 07. END PORTFOLIO MIXITUP JS
-		
+
 		// 08. START VENOBOX JS
 		$('.image-popup').venobox({
 			numeratio: true,
-			infinigall: true
+			infinigall: true,
+      bgcolor    : '#000000'
 		});
 		// 08. END VENOBOX JS
-		
+
 		// 09. START COUNTDOWN JS
 		$('.counter-feature').on('inview', function(event, visible, visiblePartX, visiblePartY) {
 			if (visible) {
@@ -105,13 +106,13 @@
 			}
 		});
 		// 09. END COUNTDOWN JS
-		
+
 		// 10. START MAGNIFIC POPUP JS
 		$('.video-play').magnificPopup({
             type: 'iframe'
         });
 		// 10. END MAGNIFIC POPUP JS
-		
+
         // 11. START GOOGLE MAP JS
          var myCenter=new google.maps.LatLng(-37.81137, 144.96102);
             function initialize(){
@@ -125,12 +126,12 @@
 				};
 	            var map=new google.maps.Map(document.getElementById("contactgoogleMap"),mapProp);
             }
-            google.maps.event.addDomListener(window, 'load', initialize);        
+            google.maps.event.addDomListener(window, 'load', initialize);
 		// 11. END GOOGLE MAP JS
 	});
-	
+
 	// 12. START WOW ANIMATION JS
 	new WOW().init();
 	// 12. END WOW ANIMATION JS
-	
+
 })(jQuery);
